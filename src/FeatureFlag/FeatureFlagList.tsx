ba20e31607fbdda60c627603a7c9d5720f26374c
haseena-pa <haseenakhader.uk@gmail.com>
2022-03-16 19:20:36 +0000

    Checkbox and Dropdown Changes Handling

--- src/FeatureFlag/FeatureFlagList.tsx
+++ src/FeatureFlag/FeatureFlagList.tsx
@@ -30,7 +30,58 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 function FeatureFlagList() {
+  const handleChange = (
+    ldx: number,
+    gdx: number,
+    fdx: number,
+    cdx: number,
+    event: any
+  ) => {
+    if (
+      cdx !== null &&
+      featureGroup[ldx].groups[gdx].items[fdx].children &&
+      featureGroup[ldx].groups[gdx].items[fdx].children[cdx]
+    ) {
+      featureGroup[ldx].groups[gdx].items[fdx].children[cdx].value =
+        event.target.value;
+    }
+    setfeatureGroup([...featureGroups]);
+  };
   const [featureGroup, setfeatureGroup] = React.useState(featureGroups);
+
+  const handleSwitch = (
+    ldx: number,
+    gdx: number,
+    fdx: number,
+    cdx: number,
+    event: any
+  ) => {
+    if (
+      cdx !== null &&
+      featureGroup[ldx].groups[gdx].items[fdx].children &&
+      featureGroup[ldx].groups[gdx].items[fdx].children[cdx]
+    ) {
+      featureGroup[ldx].groups[gdx].items[fdx].children[cdx].enabled =
+        event.target.checked;
+      featureGroup[ldx].groups[gdx].items[fdx].enabled =
+        event.target.checked &&
+        !featureGroup[ldx].groups[gdx].items[fdx].enabled
+          ? true
+          : featureGroup[ldx].groups[gdx].items[fdx].enabled;
+    } else {
+      featureGroup[ldx].groups[gdx].items[fdx].enabled = event.target.checked;
+      if (
+        !event.target.checked &&
+        featureGroup[ldx].groups[gdx].items[fdx].hasChildren
+      ) {
+        featureGroup[ldx].groups[gdx].items[fdx].children.map(
+          (child) => (child.enabled = false)
+        );
+      }
+    }
+
+    setfeatureGroup([...featureGroups]);
+  };
 
   return (
     <ThemeProvider theme={darkTheme}>
@@ -38,54 +89,60 @@ function FeatureFlagList() {
         <Box sx={{ flexGrow: 1, display: "flex", flexWrap: "wrap" }}>
           {featureGroups.map((mainGroup, mainGroupIndex) => {
             return (
-              <Grid
-                container
-                spacing={{ xs: 2, md: 3 }}
-                columns={{ xs: 4, sm: 8, md: 12 }}
-                className="featureGrid"
-                key={mainGroupIndex}
-              >
-                {mainGroup.groups.map((mainFeature, mainFeatureIndex) => {
-                  return (
-                    <Grid
-                      item
-                      xs={12}
-                      sm={4}
-                      key={mainFeatureIndex}
-                      className={`${
-                        mainFeature.items.length > 1 ? "flexWrap" : ""
-                      }`}
-                    >
-                      <Item>
-                        {mainFeature.items.map(
-                          (subFeature, subFeatureIndex) => (
-                            <FormGroup
-                              className="lowMargin"
-                              key={subFeatureIndex}
-                            >
-                              {subFeature.hasChildren ? (
-                                <ExpandableFeature
-                                  feature={subFeature}
-                                  index={subFeatureIndex}
-                                  ldx={mainFeatureIndex}
-                                  gdx={mainFeatureIndex}
-                                />
-                              ) : (
-                                <NonExpandableFeature
-                                  feature={subFeature}
-                                  index={subFeatureIndex}
-                                  ldx={mainFeatureIndex}
-                                  gdx={mainFeatureIndex}
-                                />
-                              )}
-                            </FormGroup>
-                          )
-                        )}
-                      </Item>
-                    </Grid>
-                  );
-                })}
-              </Grid>
+              <div key={mainGroupIndex} className="divWrap">
+                <h4 className="sectionTitle">{mainGroup.sectionName}</h4>
+                <Grid
+                  container
+                  spacing={{ xs: 2, md: 3 }}
+                  columns={{ xs: 4, sm: 8, md: 12 }}
+                  className="featureGrid"
+                  key={mainGroupIndex}
+                >
+                  {mainGroup.groups.map((mainFeature, mainFeatureIndex) => {
+                    return (
+                      <Grid
+                        item
+                        xs={12}
+                        sm={4}
+                        key={mainFeatureIndex}
+                        className={`${
+                          mainFeature.items.length > 1 ? "flexWrap" : ""
+                        }`}
+                      >
+                        <Item>
+                          {mainFeature.items.map(
+                            (subFeature, subFeatureIndex) => (
+                              <FormGroup
+                                className="lowMargin"
+                                key={subFeatureIndex}
+                              >
+                                {subFeature.hasChildren ? (
+                                  <ExpandableFeature
+                                    feature={subFeature}
+                                    index={subFeatureIndex}
+                                    ldx={mainFeatureIndex}
+                                    gdx={mainGroupIndex}
+                                    handleToggle={handleSwitch}
+                                    handleChange={handleChange}
+                                  />
+                                ) : (
+                                  <NonExpandableFeature
+                                    feature={subFeature}
+                                    index={subFeatureIndex}
+                                    ldx={mainGroupIndex}
+                                    gdx={mainFeatureIndex}
+                                    handleToggle={handleSwitch}
+                                  />
+                                )}
+                              </FormGroup>
+                            )
+                          )}
+                        </Item>
+                      </Grid>
+                    );
+                  })}
+                </Grid>
+              </div>
             );
           })}
         </Box>
